39172ce13bfdd9346336702f85dde8ddc21e0c2b
Benjamin Worwa <worwa.benjamin@gmail.com>
2026-06-11 18:24:19 -0400

    Updated pre-cache list

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
         theme_color: '#00857C',
       },
       workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg,mp4}'],
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,mp4,jpg,jpeg,webp,woff2}'],
         maximumFileSizeToCacheInBytes: 105000000
       }
     })
